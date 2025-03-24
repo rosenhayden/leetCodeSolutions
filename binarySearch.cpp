7ab7c003b8652be805babad963579fe6ce833248
Hayden Rosen <haydenrosen@proton.me>
2025-03-24 21:05:30 +0000

    binary search leetcode

---
+++ binarySearch.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int a = 0, b = nums.size()-1;
+        while ( a <= b) {
+            int k = (a + b) / 2;
+            if (nums[k] == target) {
+                return k;
+            }
+            if (nums[k] > target) {
+                b = k-1;
+            }
+            else {
+                a = k+1;
+            }
+        }
+        return -1;
+    }
+};
